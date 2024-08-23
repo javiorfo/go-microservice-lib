9ceb5e99faeb177c8b44c24873295f54e6c28b00
Javier Orfo <javierorfo@prontonmail.com>
2024-08-23 11:35:42 -0300

    auditory bson

--- auditory/auditory.go
+++ auditory/auditory.go
@@ -10,13 +10,13 @@ import (
 // @Description creates data fields for recording auditory
 // @ID auditory.Auditable
 type Auditable struct {
-	CreatedBy      string     `json:"-"`
-	LastModifiedBy *string    `json:"-"`
-	CreateDate     time.Time  `json:"-" gorm:"autoCreateTime"`
-	LastModified   *time.Time `json:"-" gorm:"autoUpdateTime"`
+    CreatedBy      string     `json:"-" bson:"createdBy"`
+	LastModifiedBy *string    `json:"-" bson:"lastModifiedBy"`
+	CreateDate     time.Time  `json:"-" gorm:"autoCreateTime" bson:"createDate"`
+	LastModified   *time.Time `json:"-" gorm:"autoUpdateTime" bson:"lastModified"`
 }
 
-func MapFieldToColumn(fieldName string) string {
+func (Auditable) MapFieldToSQLColumn(fieldName string) string {
 	fieldToColumnMap := map[string]string{
 		"CreatedBy":      "created_by",
 		"CreateDate":     "create_date",
